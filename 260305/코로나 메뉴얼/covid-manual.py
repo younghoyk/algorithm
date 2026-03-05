4e393dad87be310bc63568795fba5f6adff09604
younghoyk <75840396+younghoyk@users.noreply.github.com>
2026-03-05 16:46:07 +0900

    Title: 코로나 메뉴얼, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 260305/코로나 메뉴얼/covid-manual.py
+++ 260305/코로나 메뉴얼/covid-manual.py
@@ -1,16 +1,21 @@
-c1,t1=input().split()
-c2,t3=input().split()
-c3,t3=input().split()
-count=0
+# 변수 선언 및 입력
+inp = input().split()
+c1, t1 = inp[0], int(inp[1])
+inp = input().split()
+c2, t2 = inp[0], int(inp[1])
+inp = input().split()
+c3, t3 = inp[0], int(inp[1])
 
-if c1=="Y" and c2=="Y" :
-    if int(t1)>=37 and int(t2)>37 :
+# A가 2명 이상인지 판단하기
+if c1 == 'Y' and t1 >= 37:
+    # 첫 번째 사람이 A라면, 남은 두 사람 중 한 사람이라도 A면 됩니다.
+    if (c2 == 'Y' and t2 >= 37) or (c3 == 'Y' and t3 >= 37):
         print("E")
-elif c1=="Y" and c3=="Y" :
-    if int(t1)>=37 and int(t3)>37 :
+    else:
+        print("N")
+else:
+    # 첫 번째 사람이 A가 아니라면, 남은 두 사람 모두 A여야만 합니다.
+    if (c2 == 'Y' and t2 >= 37) and (c3 == 'Y' and t3 >= 37):
         print("E")
-elif c2=="Y" and c3=="Y" :
-    if int(t2)>=37 and int(t3)>37 :
-        print("E")
-else :
-    print("N")
+    else:
+        print("N")
