159f14344aa1f8ceff7e2c2d0394b68e8dc4caa9
younghoyk <75840396+younghoyk@users.noreply.github.com>
2026-03-05 16:43:32 +0900

    Title: 코로나 메뉴얼, Time: 75ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 260305/코로나 메뉴얼/covid-manual.py
+++ 260305/코로나 메뉴얼/covid-manual.py
@@ -3,13 +3,13 @@ c2,t3=input().split()
 c3,t3=input().split()
 count=0
 
-if c1="Y" and c2="Y" :
+if c1=="Y" and c2=="Y" :
     if int(t1)>=37 and int(t2)>37 :
         print("E")
-elif c1="Y" and c3="Y" :
+elif c1=="Y" and c3=="Y" :
     if int(t1)>=37 and int(t3)>37 :
         print("E")
-elif c2="Y" and c3="Y" :
+elif c2=="Y" and c3=="Y" :
     if int(t2)>=37 and int(t3)>37 :
         print("E")
 else :
