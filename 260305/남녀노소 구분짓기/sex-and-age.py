e9edf4e2341ebe4f5fef197754d1236192c81857
younghoyk <75840396+younghoyk@users.noreply.github.com>
2026-03-05 16:33:01 +0900

    Title: 남녀노소 구분짓기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260305/남녀노소 구분짓기/sex-and-age.py
@@ -0,0 +1,12 @@
+g=int(input())
+a=int(input())
+if g==0 :
+    if a>=19 :
+        print("MAN")
+    else :
+        print("BOY")
+else :
+    if a>=19 :
+        print("WOMAN")
+    else:
+        print("GIRL")
