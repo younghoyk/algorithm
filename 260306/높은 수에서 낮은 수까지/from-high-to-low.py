593b40cc7459e220a075f04b2a4d35f8c4624559
younghoyk <75840396+younghoyk@users.noreply.github.com>
2026-03-06 19:45:34 +0900

    Title: 높은 수에서 낮은 수까지, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260306/높은 수에서 낮은 수까지/from-high-to-low.py
+++ 260306/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -1,3 +1,7 @@
 A,B=map(int,input().split())
-for i in range(B,A-1,-1):
-    print(i,end=" ")
+if A<=B :
+    for i in range(B,A-1,-1):
+        print(i,end=" ")
+else :
+    for i in range(A,B-1,-1):
+        print(i,end=" ")
