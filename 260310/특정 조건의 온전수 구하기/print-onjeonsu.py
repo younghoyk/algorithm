e73530dc7f55faac957fb424cf6994c73722dca7
younghoyk <75840396+younghoyk@users.noreply.github.com>
2026-03-10 00:11:32 +0900

    Title: 특정 조건의 온전수 구하기, Time: 74ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 260310/특정 조건의 온전수 구하기/print-onjeonsu.py
+++ 260310/특정 조건의 온전수 구하기/print-onjeonsu.py
@@ -1,7 +1,9 @@
 N=int(input())
+ans=[]
 for i in range(1,N+1):
     if i %2 ==0 or (i%3==0 and i%9!=0):
         continue
     if str(i)[0]=="5" :
         continue
-    print(i,end=" ")
+    ans.append(spr(i))
+print(f"{' '.join(ans))")
