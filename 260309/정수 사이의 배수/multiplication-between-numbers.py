1bffdeb6f5a8eb877807291ac2cc6e371e1473d8
younghoyk <75840396+younghoyk@users.noreply.github.com>
2026-03-09 22:53:10 +0900

    Title: 정수 사이의 배수, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260309/정수 사이의 배수/multiplication-between-numbers.py
+++ 260309/정수 사이의 배수/multiplication-between-numbers.py
@@ -1,8 +1,10 @@
 A,B=map(int,input().split())
-sum_val
+sum_val=0
+sum_num=0
 for i in range(A,B+1) :
     if i % 5 ==0 or i % 7 ==0 :
         sum_val += i
-print(f"{sum_val} {sum_val/(B-A):.1f}")
+        sum_num +=1
+print(f"{sum_val} {sum_val/sum_num:.1f}")
 
     
