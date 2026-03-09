0e36a0b9608cb9cec82485e8adb17b4613c05bcd
younghoyk <75840396+younghoyk@users.noreply.github.com>
2026-03-10 00:35:35 +0900

    Title: 별표 출력하기 12, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 260310/별표 출력하기 12/print-star-12.py
+++ 260310/별표 출력하기 12/print-star-12.py
@@ -1,10 +1,11 @@
-N=int(input().split())
-matrix=[[0]*N for _ in range(N)]
+N=int(input())
+matrix=[[" "]*N for _ in range(N)]
 for i in range(N):
     for j in range(N):
         matrix[0][j]="*"
-        if i %2 ==1 and i<j :
+        if j %2 ==1 and i<=j :
             matrix[i][j]="*"
 for i in range(N):
     for j in range(N):
         print(matrix[i][j],end=" ")
+    print()
