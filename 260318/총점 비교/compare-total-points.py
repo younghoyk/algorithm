6eeb13bb8809bd02fbeba47cd48a9392737a8205
younghoyk <75840396+younghoyk@users.noreply.github.com>
2026-03-18 15:00:36 +0900

    Title: 총점 비교, Time: 80ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 260318/총점 비교/compare-total-points.py
@@ -0,0 +1,15 @@
+class student :
+    def __init__(self,name,a,b,c) :
+        self.name=name
+        self.a=int(a)
+        self.b=int(b)
+        self.c=int(c)
+
+N=int(input())
+students=[]
+for _ in range(N):
+    a,b,c=input().split()
+    students.append(student(a,b,c))
+student.sort(key=lambda x : x.a+x.b+x.c)
+for i in students:
+    print(i.name, i.a, i.b, i.c)
