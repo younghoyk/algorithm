a25ee4bc18cfcfe5a161e7bea75e9ed16f39f773
younghoyk <75840396+younghoyk@users.noreply.github.com>
2026-03-19 01:05:22 +0900

    Title: 문자에 따른 명령 2, Time: 59ms, Memory: 19MB, Status: Passed - Codetree

--- 260319/문자에 따른 명령 2/text-based-commands2.py
+++ 260319/문자에 따른 명령 2/text-based-commands2.py
@@ -3,10 +3,10 @@ direction=0
 command=list(input())
 dx,dy=[0,1,0,-1],[1,0,-1,0]
 
-for i in range(len(command)) :
-    if command =="L":
+for i in command :
+    if i =="L":
         direction= (direction+3) %4
-    elif command == "R" :
+    elif i == "R" :
         direction=(direction+1)%4
     else :
         x,y=x+dx[direction],y+dy[direction]
