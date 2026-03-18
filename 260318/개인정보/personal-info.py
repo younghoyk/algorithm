c2c87a70650641827734ea83f95ab7fb1ccf469a
younghoyk <75840396+younghoyk@users.noreply.github.com>
2026-03-18 23:54:58 +0900

    Title: 개인정보, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260318/개인정보/personal-info.py
+++ 260318/개인정보/personal-info.py
@@ -1,5 +1,5 @@
 students = [tuple(input().split()) + (i + 1,) for i in range(5)]
-
+students = [(student[0],int(student[1]),student[2]) for student in students]
 students.sort(key=lambda x : x[0])
 
 print("name")
@@ -7,7 +7,7 @@ for student in students:
     print(*student)
 
 students.sort(key=lambda x : -x[1])
-
+print()
 print("height")
 for student in students:
     print(*student)
