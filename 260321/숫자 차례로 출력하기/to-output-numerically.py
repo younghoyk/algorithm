876b193f9ed9c5beaacb0e269397d08b5620833e
younghoyk <75840396+younghoyk@users.noreply.github.com>
2026-03-21 20:41:44 +0900

    Title: 숫자 차례로 출력하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260321/숫자 차례로 출력하기/to-output-numerically.py
+++ 260321/숫자 차례로 출력하기/to-output-numerically.py
@@ -3,14 +3,14 @@ n = int(input())
 def prt1(n) :
     if n ==0 :
         return
-    prt(n-1)
+    prt1(n-1)
     print(n,end=" ")
 
 def prt2(n) :
     if n ==0 :
         return
     print(n,end=" ")
-    prt(n-1)
+    prt2(n-1)
 
 prt1(n)
 print()
